8f39f3abb425280018e29d34afbabe0fe03af4e3
hqsfily <530092543@qq.com>
2018-10-14 12:00:55 +0000

    第一章结束

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','关于我们')
+@section('content')
+	<h2>关于我们</h2>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '帮助')
+@section('content')
+	<h3>帮助页面</h3>
+@stop
